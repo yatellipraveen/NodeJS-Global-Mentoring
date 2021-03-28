88688965379c46d1b7adbe3bf8300974faafcda8
yatellipraveen <yatellipraveen@gmail.com>
2021-03-28 12:22:21 +0530

    Home Task for PR

---
+++ HomeTask-1/task2/csvtojson.js
@@ -0,0 +1,21 @@
+//Task 1.2 convert csv file to json 
+const csv=require('csvtojson');
+const fs = require('fs');
+
+const inputPath = 'task2/inputcsv/inputfile.csv';
+const outputPath = 'task2/outputtxt/output.txt';
+
+
+const readstream = fs.createReadStream(inputPath);
+const writeStream = fs.createWriteStream(outputPath);
+
+csv()
+.fromStream(readstream)
+.then((jsonObj)=>{
+    const jsonResult = JSON.stringify(jsonObj)
+    writeStream.write(jsonResult);
+});
+
+readstream.on('error', (error) =>{
+    console.log("Input File Not Found")
+})
